f9a7c274e26ff655e174ff02fc4a0fc0909ab37c
MatiasCandotti <maticandotti02@outlook.com>
2023-06-07 17:18:16 -0300

    Clase 1
    package-lock auto config
    static add image
    index.ts changes
    > pixiJS v7 adapt
    > cout string + img size

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Assets, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +9,26 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+//Loader.shared.add({url:"./image.png", name: "myImage"});
+//Loader.shared.add({url:"./clampy.png", name: "Clampy"});
+Assets.add("Clampy", "./clampy.png");
+Assets.add("myImage", "./image.png");
 
-clampy.anchor.set(0.5);
+//Loader.shared.onComplete.add(()=>{
+Assets.load(["Clampy","myImage"]).then( ()=>{
+	const clampy: Sprite = Sprite.from("Clampy");
+	const image: Sprite = Sprite.from("myImage");
+	
+	console.log("Image size:", image.width, image.height);
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+	clampy.anchor.set(0.5);
+	clampy.x = app.screen.width / 2;
+	clampy.y = app.screen.height / 2;
 
-app.stage.addChild(clampy);
+	image.x = 0;
+	image.y = 0;
+	
+	app.stage.addChild(clampy);
+	app.stage.addChild(image);
+})	
+//Loader.shared.load();
