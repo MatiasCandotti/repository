dc2d66af502efe28362035dc73921692851563c2
MatiasCandotti <maticandotti02@outlook.com>
2023-06-07 19:11:29 -0300

    Clase 2
    index
    > change BG color
    > variable screen scale (black bars)
    > ERR screen always centered
    > ideal size 680x480 to 800x600

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,32 @@ const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	backgroundColor: 0x4145ed,
+	width: 800,
+	height: 600
 });
+
+window.addEventListener("resize", ()=>{
+	const scaleX = (window.innerWidth/app.screen.width);
+	const scaleY = (window.innerHeight/app.screen.height);
+	const scale = Math.min(scaleX, scaleY);
+
+	const displayWidth = Math.round(app.screen.width * scale);
+	const displayHeight = Math.round(app.screen.height * scale);
+
+//	const marginH = Math.round((window.innerWidth - displayWidth)/2);
+//	const marginV = Math.round((window.innerHeight - displayHeight)/2);
+
+	app.view.style!.width = displayWidth + "px";
+	app.view.style!.height = displayHeight + "px";
+
+//	app.view.style!.marginLeft = marginH + "px";
+//	app.view.style!.marginRight = marginH + "px";
+
+//	app.view.style!.marginTop = marginV + "px";
+//	app.view.style!.marginBottom = marginV + "px";
+});
+window.dispatchEvent(new Event("resize"));
 
 Assets.add("Clampy", "./clampy.png");
 Assets.add("myImage", "./image.png");
