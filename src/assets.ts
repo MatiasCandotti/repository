d9d835d855933200afdc44154f2a61fc00d6ea73
MatiasCandotti <maticandotti02@outlook.com>
2023-06-22 18:44:11 -0300

    Clase 3
    Created assets.ts
    
    Random errors. old code somewhere?
    No idea

---
+++ src/assets.ts
@@ -0,0 +1,8 @@
+export const assets = {
+    clampy: "./Clampy",
+    image: "./myImage",
+    buttons: "./buttonYN",
+    man: "./manImage",
+    anchorGlobal: "./anchorGlobal",
+    anchorLocal: "./anchorLocal",
+};
